c661ef68c08c2ef4c445195851e48310549239a3
Whiz7z <gjevstratov@gmail.com>
2023-07-19 23:10:26 +0100

    route

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
-import MainBlock from "../components/MainBlock";
+"use client";
 import Auth from "@/components/Auth";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
-export default async function Home() {
+export default function Home() {
+  const router = useRouter();
+  const { data: session, status } = useSession<any>();
+  console.log(status);
+  if (status === "authenticated") {
+    router.push("/profile");
+  }
   return <Auth />;
 }
--- app/profile/addExpense/page.tsx
+++ app/profile/addExpense/page.tsx
@@ -1,10 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Types from "@/components/serverComponents/Types";
 import Input from "@/components/Input";
-import { useSession } from "next-auth/react";
-import { getServerSession } from "next-auth";
-import { authOption } from "@/app/api/auth/[...nextauth]/route";
-import MoveForward from "@/components/navigation/MoveForward";
 import MoveBack from "@/components/navigation/MoveBack";
 type Props = {};
 
